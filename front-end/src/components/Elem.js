3b163e81d7f45512de0cf924e0ea40aea46e29fc
Torstein Thomassen <torstein1996@hotmail.com>
2019-06-20 21:04:29 +0200

    init

---
+++ front-end/src/components/Elem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Elem = ({ todo, index, handleDelete }) => {
+    return (
+        <div>
+            {todo.text}
+            <button onClick={() => handleDelete(index)}>X</button>
+        </div>
+    )
+}
+
+export default Elem
